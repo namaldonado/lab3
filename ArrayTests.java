84c914ac4b501d003ff40e02fdac9002c2e593f5
namaldonado <namaldonado@ucsd.edu>
2023-04-20 19:37:24 -0700

    removed the bugs in ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedInPlacept2(){
+    int[] input1={3,2,6,7,2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2,7,6,2,3}, input1);
+  }
+
+
+  @Test
+  public void testReversedpt2() {
+    int[] input1 = {9 ,1};
+    assertArrayEquals(new int[]{ 1, 9}, ArrayExamples.reversed(input1));
+  }
 }
